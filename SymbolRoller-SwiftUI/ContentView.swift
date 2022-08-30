0eab7a0bb8b18929d390979ab2944a4ea9e0d3fc
Jun <wnsgur4092@gmail.com>
2022-08-31 00:51:12 +1000

    Initial Commit

---
+++ SymbolRoller-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by JunHyuk Lim on 31/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SymbolRoller-SwiftUI/SymbolRoller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRoller_SwiftUIApp.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by JunHyuk Lim on 31/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRoller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
