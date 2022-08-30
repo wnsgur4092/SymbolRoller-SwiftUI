7eceadefe1a2ff1f42212577ac0685722366963b
Jun <wnsgur4092@gmail.com>
2022-08-31 01:14:09 +1000

    @State

--- SymbolRoller-SwiftUI/ContentView.swift
+++ SymbolRoller-SwiftUI/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let symbols : [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    @State var imageName : String = "sun.min"
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            
+            Spacer()
+            
+            Text(imageName)
+                .font(.system(size: 40, weight: .bold, design: .default))
+            
+            Button{
+                imageName = symbols.randomElement()!
+            } label : {
+                HStack{
+                    Image(systemName: "arrow.3.trianglepath")
+                    VStack{
+                        Text("Reload")
+                            .font(.system(size: 30, weight: .bold, design: .default))
+                        Text("Click me to reload")
+                    }
+                }
+            }
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(.pink)
+            .cornerRadius(40)
+            
+        }
     }
 }
 
